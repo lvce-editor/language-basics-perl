4064902a85b8d9aa16b972f8df9416cd3a07342f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-10-16 23:21:20 +0200

    add basic keyword highlighting (#3)

--- src/tokenizePerl.js
+++ src/tokenizePerl.js
@@ -1,31 +1,146 @@
 /**
  * @enum number
  */
+export const State = {
+  TopLevelContent: 1,
+  InsideString: 2,
+  InsideLineComment: 3,
+}
+
+export const StateMap = {
+  [State.TopLevelContent]: 'TopLevelContent',
+}
+
+/**
+ * @enum number
+ */
 export const TokenType = {
-  Text: 1,
+  None: 1,
+  Whitespace: 2,
+  PunctuationString: 3,
+  String: 4,
+  Keyword: 5,
+  Numeric: 6,
+  Punctuation: 7,
+  VariableName: 8,
+  Comment: 885,
+  Text: 9,
 }
 
 export const TokenMap = {
+  [TokenType.None]: 'None',
+  [TokenType.Whitespace]: 'Whitespace',
+  [TokenType.PunctuationString]: 'PunctuationString',
+  [TokenType.String]: 'String',
+  [TokenType.Keyword]: 'Keyword',
+  [TokenType.Numeric]: 'Numeric',
+  [TokenType.Punctuation]: 'Punctuation',
+  [TokenType.VariableName]: 'VariableName',
+  [TokenType.Comment]: 'Comment',
   [TokenType.Text]: 'Text',
 }
 
+const RE_LINE_COMMENT = /^#.*/s
+const RE_SELECTOR = /^[\.a-zA-Z\d\-\:>]+/
+const RE_WHITESPACE = /^ +/
+const RE_CURLY_OPEN = /^\{/
+const RE_CURLY_CLOSE = /^\}/
+const RE_PROPERTY_NAME = /^[a-zA-Z\-]+\b/
+const RE_COLON = /^:/
+const RE_PROPERTY_VALUE = /^[^;\}]+/
+const RE_SEMICOLON = /^;/
+const RE_COMMA = /^,/
+const RE_ANYTHING = /^.+/s
+const RE_ANYTHING_UNTIL_CLOSE_BRACE = /^[^\}]+/
+const RE_QUOTE_DOUBLE = /^"/
+const RE_STRING_DOUBLE_QUOTE_CONTENT = /^[^"]+/
+const RE_KEYWORD =
+  /^(?:case|continue|default|die|no|else|elsif|eval|exit|for|foreach|given|goto|if|last|next|redo|require|return|select|switch|unless|until|use|wait|when|while)\b/
+
+const RE_VARIABLE_NAME = /^[a-zA-Z\_]+/
+const RE_PUNCTUATION = /^[:,;\{\}\[\]\.=\(\)<>]/
+const RE_NUMERIC = /^\d+/
+
 export const initialLineState = {
-  state: 0,
+  state: State.TopLevelContent,
   tokens: [],
 }
 
 /**
- * @param {string} line
+ *
+ * @param {any} lineStateA
+ * @param {any} lineStateB
+ * @returns
  */
-export const tokenizeLine = (line) => {
-  const tokens = [
-    {
-      type: TokenType.Text,
-      length: line.length,
-    },
-  ]
+export const isEqualLineState = (lineStateA, lineStateB) => {
+  return lineStateA.state === lineStateB.state
+}
+
+export const hasArrayReturn = true
+
+/**
+ * @param {string} line
+ * @param {any} lineState
+ */
+export const tokenizeLine = (line, lineState) => {
+  let next = null
+  let index = 0
+  let tokens = []
+  let token = TokenType.None
+  let state = lineState.state
+  while (index < line.length) {
+    const part = line.slice(index)
+    switch (state) {
+      case State.TopLevelContent:
+        if ((next = part.match(RE_WHITESPACE))) {
+          token = TokenType.Whitespace
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_KEYWORD))) {
+          token = TokenType.Keyword
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_PUNCTUATION))) {
+          token = TokenType.Punctuation
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_VARIABLE_NAME))) {
+          token = TokenType.VariableName
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_NUMERIC))) {
+          token = TokenType.Numeric
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_QUOTE_DOUBLE))) {
+          token = TokenType.PunctuationString
+          state = State.InsideString
+        } else if ((next = part.match(RE_LINE_COMMENT))) {
+          token = TokenType.Comment
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_ANYTHING))) {
+          token = TokenType.Text
+          state = State.TopLevelContent
+        } else {
+          part //?
+          throw new Error('no')
+        }
+        break
+      case State.InsideString:
+        if ((next = part.match(RE_QUOTE_DOUBLE))) {
+          token = TokenType.PunctuationString
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_STRING_DOUBLE_QUOTE_CONTENT))) {
+          token = TokenType.String
+          state = State.InsideString
+        } else {
+          throw new Error('no')
+        }
+        break
+      default:
+        throw new Error('no')
+    }
+    const tokenLength = next[0].length
+    index += tokenLength
+    tokens.push(token, tokenLength)
+  }
   return {
-    state: 0,
+    state,
     tokens,
   }
 }
